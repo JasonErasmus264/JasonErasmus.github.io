96704857df22448ba51b9e2d7660e7b57e0e8981
JasonErasmus264 <jasonerasmus264@gmail.com>
2024-07-16 11:12:33 +0200

    Added Work/Play Switch for Projects

--- script.js
+++ script.js
@@ -4,3 +4,22 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const inputs = document.querySelectorAll('.switch input');
+    const workLabel = document.querySelector('.option.work');
+    const playLabel = document.querySelector('.option.play');
+
+    inputs.forEach(input => {
+        input.addEventListener('change', () => {
+            if (input.id === 'work') {
+                workLabel.style.fontWeight = '700';
+                playLabel.style.fontWeight = '400';
+            } else if (input.id === 'play') {
+                workLabel.style.fontWeight = '400';
+                playLabel.style.fontWeight = '700';
+            }
+        });
+    });
+});
